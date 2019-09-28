b8371f93d57961a037ad6bcc3e97340379fdbedb
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-09-28 14:29:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -42,3 +42,12 @@ def reduce_to_total(source_array, starting_point = 0)
   end
   total
 end
+
+def reduce_to_all_true(source_array)
+  i = 0
+  while i < source_array.size
+    if source_array[i] == false
+      return false
+  end
+  true
+end
